c058693e1a675d88b6ef4cc99d8d771e5087b9b6
yxeque <yxequel@gmail.com>
2023-12-28 11:49:31 +0800

    Add playRound function and main game logic

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ let getComputerChoice = (computerChoice) => {
 }
 
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+
+    if (playerSelection == computerSelection) {
+        console.log("It's a Tie!")
+    } else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+    } else if (playerSelection == "PAPER" && computerSelection == "SCISSOR") {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+    } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCKS") {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+    }
+}
+
+const playerSelection = "rOcK";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
