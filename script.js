59341933f7f821968230a52e3ef72de1e371be25
yxeque <yxequel@gmail.com>
2023-12-28 15:58:54 +0800

    Refactor code inside game function

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
 
-// GET COMPUTER CHOICE
-let getComputerChoice = (computerChoice) => {
-    computerChoice = Math.floor((Math.random() * 3) + 1);
-    switch (computerChoice) {
-        case 1:
-            computerChoice = 'ROCK';
-            break;
-        case 2:
-            computerChoice = 'PAPER';
-            break;
-        case 3:
-            computerChoice = 'SCISSORS';
-            break;
+function game() {
+    // GET COMPUTER CHOICE
+    function getComputerChoice(computerChoice) {
+        computerChoice = Math.floor((Math.random() * 3) + 1);
+        switch (computerChoice) {
+            case 1:
+                computerChoice = 'ROCK';
+                break;
+            case 2:
+                computerChoice = 'PAPER';
+                break;
+            case 3:
+                computerChoice = 'SCISSORS';
+                break;
+        }
+        return computerChoice;
     }
-    return computerChoice;
+
+    console.log(getComputerChoice());
+
+    function playRound(playerSelection, computerSelection) {
+        playerSelection = playerSelection.toUpperCase();
+    
+        if (playerSelection == computerSelection) {
+            console.log("It's a Tie!")
+        } else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+        } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+        } else if (playerSelection == "PAPER" && computerSelection == "SCISSOR") {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+        } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+        } else if (playerSelection == "SCISSORS" && computerSelection == "ROCKS") {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+        } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+        }
+    }
+
+    for (i = 1; i <= 5; i ++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("Rock, Paper, or Scissors? Choose.");
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
 }
 
-console.log(getComputerChoice());
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-
-    if (playerSelection == computerSelection) {
-        console.log("It's a Tie!")
-    } else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
-    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
-    } else if (playerSelection == "PAPER" && computerSelection == "SCISSOR") {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
-    } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
-    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCKS") {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
-    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
-    }
-}
-
-const playerSelection = prompt("Rock, Paper, or Scissors? Choose.");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game();
 
 
