a76f7c82893a35156d9f003018d9e0d9719a3d7e
yxeque <yxequel@gmail.com>
2023-12-28 16:06:44 +0800

    Add scoring logic

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
     // GET COMPUTER CHOICE
     function getComputerChoice(computerChoice) {
         computerChoice = Math.floor((Math.random() * 3) + 1);
@@ -17,25 +20,36 @@ function game() {
         return computerChoice;
     }
 
-    console.log(getComputerChoice());
-
     function playRound(playerSelection, computerSelection) {
         playerSelection = playerSelection.toUpperCase();
     
         if (playerSelection == computerSelection) {
             console.log("It's a Tie!")
+
         } else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
             console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+            computerScore++;
+
         } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
             console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+            playerScore++;
+
         } else if (playerSelection == "PAPER" && computerSelection == "SCISSOR") {
             console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+            computerScore++;
+
         } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
             console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+            playerScore++;
+
         } else if (playerSelection == "SCISSORS" && computerSelection == "ROCKS") {
             console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`)
+            computerScore++;
+
         } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
             console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+            playerScore++;
+
         }
     }
 
@@ -43,6 +57,14 @@ function game() {
         const computerSelection = getComputerChoice();
         const playerSelection = prompt("Rock, Paper, or Scissors? Choose.");
         console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (playerScore == computerScore) {
+        console.log(`It's a Tie! ${playerScore} to ${computerScore}`)
+    } else if (playerScore > computerScore) {
+        console.log(`You Win! ${playerScore} to ${computerScore}`)
+    } else {
+        console.log(`You Lose! ${computerScore} to ${playerScore}`)
     }
 
 }
