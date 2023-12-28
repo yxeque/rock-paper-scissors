39a9014b5063deb95dd5750c158b2498bd3c01ba
yxeque <yxequel@gmail.com>
2023-12-28 11:40:17 +0800

    Update getComputerChoice;  reassign random val to rock-paper-scissors

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
+// GET COMPUTER CHOICE
 let getComputerChoice = (computerChoice) => {
     computerChoice = Math.floor((Math.random() * 3) + 1);
+    switch (computerChoice) {
+        case 1:
+            computerChoice = 'ROCK';
+            break;
+        case 2:
+            computerChoice = 'PAPER';
+            break;
+        case 3:
+            computerChoice = 'SCISSORS';
+            break;
+    }
     return computerChoice;
 }
 
